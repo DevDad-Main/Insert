3c803bc22d84445cf764039e744d24c7a3c296b8
Oliver Metz <softwaredevdad@gmail.com>
2026-01-09 22:12:11 +0100

    Update: schema to make our new user objects. setup .env with new neondb, setup clerk webhooks via tunnel proxys and setup our api webhook.

---
+++ src/playground.ts
@@ -0,0 +1,11 @@
+import { db } from "./server/db";
+
+await db.user.create({
+  data: {
+    emailAddress: "test@example.com",
+    firstName: "Test",
+    lastName: "User",
+  },
+});
+
+console.log("Created user");
